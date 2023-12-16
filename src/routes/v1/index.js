4269f30d45352486274350f770a9b7abfaf1d9b2
gssp4167 <gssp4167@gmail.com>
2023-12-16 16:51:46 +0530

    Removed unrelated files

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const {InfoController}=require('../../controllers');
-const airplaneRoutes=require('./airplane-routes');
 
 // console.log("Inside v1Routes");
-router.use('/airplanes',airplaneRoutes);
 
 router.get('/info',    InfoController.info);
 

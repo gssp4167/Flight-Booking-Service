fda9abcc03b9d17c1463170054382e3a73a88e17
gssp4167 <gssp4167@gmail.com>
2023-12-31 21:52:02 +0530

    Started booking implementation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const {InfoController}=require('../../controllers');
-
+const bookingRoutes=require('./booking-router')
 // console.log("Inside v1Routes");
 
 router.get('/info',    InfoController.info);
 
+router.use('/bookings',bookingRoutes);
 module.exports=router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,1 +1,28 @@
-module.exports={}
+const {StatusCodes}=require('http-status-codes');
+const axios=require('axios');
+const {BookingRepository}=require('../repositories');
+const db=require('../models');
+const {ServerConfig}=require('../config');
+const AppError=require('../utils/errors/app-error');
+
+
+async function createBooking(data){
+    try {
+        const result= await db.sequelize.transaction(async function bookingImpl(t){
+            const flight=await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            const flightData=flight.data.data;
+            if(data.noOfSeats > flightData.totalSeats){
+                throw new AppError('Not enough seats available',StatusCodes.BAD_REQUEST); 
+            }
+            return true;
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
+module.exports={
+    createBooking
+}
